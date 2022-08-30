45099ba7f150e0bd518be2282daf4d5064a7a174
Nikita <ndemidovn@gmail.com>
2022-08-30 08:37:48 +0300

    carousel adaptive

--- src/js/script.js
+++ src/js/script.js
@@ -1,7 +1,20 @@
 $(document).ready(function(){
   $('.carousel__inner').slick({
     speed: 1200,
-    adaptiveHeight: true,
-    prevArrow: '<button type="button" class="slick-prev"><img src=''></button>'
+    autoplay: true,
+    autoplaySpeed: 2000,
+    // adaptiveHeight: true,
+    prevArrow: '<button type="button" class="slick-prev"><img src="img/arrow-left.png"></button>',
+    nextArrow: '<button type="button" class="slick-next"><img src="img/arrow-right.png"></button>',
+    responsive: [
+      {
+        breakpoint: 992,
+        settings: {
+          // dots: true,
+          arrows: false
+        }
+      }
+      
+    ]
   });
 });
