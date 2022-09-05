e361a5d97c55f4cfeac72ae8838022f1a8b7b0f7
Nikita <ndemidovn@gmail.com>
2022-09-05 06:54:40 +0300

    form validation

--- src/js/script.js
+++ src/js/script.js
@@ -50,4 +50,29 @@ $(document).ready(function(){
     })
   });
 
+  function validateForms(form){
+    $(form).validate({
+      rules: {
+        name: 'required',
+        phone: 'required',
+        email: {
+          required: true,
+          email: true
+        }
+      },
+      messages: {
+        name: 'Пожалуйста, введите свое имя',
+        phone: 'Пожалуйста, введите свой номер телефона',
+        email: {
+          required: 'Пожалуйста, введите свой email',
+          email: 'Неправильно введен адрес почты'
+        }
+      }
+  
+    });
+  };
+
+  validateForms('#consultation-form');
+  validateForms('#consultation form');
+  validateForms('#order form');
 });
